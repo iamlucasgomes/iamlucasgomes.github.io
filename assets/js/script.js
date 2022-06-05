a02dca3e68ded7c79b7fc1bdeab00e65f9fa44d7
iamlucasgomes <lucas.devjs@gmail.com>
2022-06-05 03:02:36 -0300

    conclusão da pagina index ptBR, inicio da pagina contato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+function showModal(){
+    var element = getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+const BUTTON = document.getElementById("button");
+
+BUTTON.addEventListener("click", showModal);
+
 date = new Date();
 year = date.getFullYear();
 document.getElementById("assinatura").innerHTML = "L U C A S &ensp; G O M E S © &ensp;" + year;
